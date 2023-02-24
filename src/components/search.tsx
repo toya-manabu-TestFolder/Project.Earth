fef5dfd5f4c226d67926e5c33d6c65702435c07d
Sakamoto Mayu <mayu.sakamoto@rakus-partners.co.jp>
2023-02-24 13:58:45 +0900

    検索欄のjsxを作りました。

---
+++ src/components/search.tsx
@@ -0,0 +1,11 @@
+export default function Search() {
+  return (
+    <div>
+      <form onSubmit={}>
+        <label htmlFor="search">商品名から生産者を探す</label>
+        <input onChange={} type="text" id="search" name="search" />
+        <button type="submit">検索</button>
+      </form>
+    </div>
+  );
+}
