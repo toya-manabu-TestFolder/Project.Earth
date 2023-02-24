084368bb9943d456efbaca6cebe130e65ec6e392
Takao Shishido <takao.shishido@rakus-partners.co.jp>
2023-02-24 11:38:11 +0900

    →compornentsディレクトリ price.tsxを作成

---
+++ src/components/price.tsx
@@ -0,0 +1,3 @@
+export default function Price() {
+  return <p>ここに価格が入る</p>;
+}
--- src/pages/example.tsx
+++ src/pages/example.tsx
@@ -1,14 +1,13 @@
 import Comment from "@/components/comment";
 import Photo from "@/components/photo";
 import PhotoName from "@/components/photoName";
+import Price from "@/components/price";
 
 export default function Example() {
   return (
     <>
     <div>
-      <Photo />
-      <PhotoName />
-      <Comment />
+
     </div>
     </>
   );
