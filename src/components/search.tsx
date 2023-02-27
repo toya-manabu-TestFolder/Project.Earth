47cb5a98eb851a2a389bd3713b4404e6d9f2dc96
Takao Shishido <takao.shishido@rakus-partners.co.jp>
2023-02-27 16:38:01 +0900

    searchコンポーネントへ機能実装 検索一覧ページ作成

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,19 +1,26 @@
-import { useState } from "react";
+import { useRouter } from "next/router";
+import { SyntheticEvent, useState } from "react";
 
 export default function Search() {
-  const [name, setName] = useState("");
-  const handleSubmit = function () {
-    console.log(name);
+  const [search, setSearch] = useState("");
+  const router = useRouter();
+  const handleSubmit = function (event: SyntheticEvent) {
+    event.preventDefault();
+    const uri = encodeURI(search);
+    // 確認用
+    // console.log(uri);
+    router.push(`/farmers?search=${uri}`);
   };
   return (
-   <div>
+    <div>
       <form onSubmit={(event) => handleSubmit(event)}>
         <label htmlFor="search">商品名から生産者を探す</label>
         <input
-          onChange={(e) => setName(e.target.value)}
+          onChange={(e) => setSearch(e.target.value)}
           type="text"
           id="search"
           name="search"
+          value={search}
         />
         <button type="submit">検索</button>
       </form>
