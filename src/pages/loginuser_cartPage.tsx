dc3c91c25036fbd144bdf2965d00f8786db33ec7
Toya Manabu <manabu.toya@rakus-partners.co.jp>
2023-03-01 11:28:24 +0900

    test

--- src/pages/loginuser_cartPage.tsx
+++ src/pages/loginuser_cartPage.tsx
@@ -1,13 +1,10 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import Image from "next/image";
 import styles from "../styles/cartpage.module.css";
+import { useState } from "react";
 
 export const getServerSideProps = async () => {
-  let cookie = {
-    category_id: 2,
-    user_id: "1",
-  };
-
-  const req1 = await fetch(`http://127.0.0.1:8000/cartitems`);
+  const req1 = await fetch("http://127.0.0.1:8000/cartitems");
   const cartitems = await req1.json();
   const req2 = await fetch("http://127.0.0.1:8000/items");
   const items = await req2.json();
@@ -20,23 +17,27 @@ export const getServerSideProps = async () => {
   };
 };
 
-const deleteCartItem = () => {
-  fetch();
-};
-
+// ------------------------------------------
 const loginuser_cartPage = (props: any) => {
   let cookie = {
     category_id: 2,
-    user_id: "1",
+    user_id: 1,
   };
+  const [deleteParam, setdeleteParam] = useState({
+    user_id: cookie.user_id,
+    item_id: 0,
+  });
+
   const items = props.items;
   const cartitems = props.cartitems;
   let check = [];
-  // const cartitems = props.cartitems.filter((cartData: any) => {
-  //   return Number(cookie.user_id) === cartData.user_id;
-  // });
+  const cartitemsArr = props.cartitems.filter((cartData: any) => {
+    return Number(cookie.user_id) === cartData.user_id;
+  });
   console.log(cartitems);
+  console.log(cartitemsArr);
 
+  // 価格表示用ファンクション
   const price = (id) => {
     let val;
     items.map((item) => {
@@ -46,6 +47,7 @@ const loginuser_cartPage = (props: any) => {
     });
     return val;
   };
+  // 名前表示用ファンクション
   const name = (id) => {
     let val;
     items.map((item) => {
@@ -55,14 +57,48 @@ const loginuser_cartPage = (props: any) => {
     });
     return val;
   };
+
+  // 削除用ファンクション
+  const deleteCartItem = async (id) => {
+    let deleteParam = {
+      user_id: Number(cookie.user_id),
+      item_id: id,
+    };
+    fetch("http://localhost:3000/api/cartDelete", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        //↓全部のデータを取り扱いたい時
+        Prefer: "return=representation",
+        //↓更新したいならTOKEN設定
+        Authorization: `Bearer ${process.env["POSTGREST_API_TOKEN"]}`,
+      },
+      body: JSON.stringify(deleteParam),
+    });
+  };
+
+  // 数量変更ファンクション
+  const changeItemQuantity = (event) => {
+    for (let i = 1; i <= event.target.value; i++) {
+      fetch("http://localhost:3000/api/cartInport", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(cartData),
+      });
+    }
+  };
+
+  // 下記JSX
   return (
     <div>
-      {cartitems.map((item) => {
+      {cartitemsArr.map((item) => {
         if (check.includes(item.item_id)) {
           return;
         } else {
           let count = 1;
-          for (const e of cartitems) {
+          for (const e of cartitemsArr) {
             if (item.id !== e.id && item.item_id === e.item_id) {
               count = count + 1;
             }
@@ -70,47 +106,55 @@ const loginuser_cartPage = (props: any) => {
           check.push(item.item_id);
           return (
             <>
-              <div>
-                {/* 下記から商品情報 */}
-                <div className={styles.section}>
-                  <Image
-                    src="/categoryImages/かぼちゃ.jpg"
-                    width={100}
-                    height={100}
-                    className={styles.sec3_ImageBox}
-                    alt={"野菜画像"}
-                  />
-                  <div>
-                    <p>商品名:{name(item.item_id)}</p>
-                  </div>
-                  <div>
-                    <p>価格:{price(item.item_id)}</p>
-                  </div>
+              <form>
+                <div>
+                  {/* 下記から商品情報 */}
+                  <div className={styles.section}>
+                    <Image
+                      src="/categoryImages/かぼちゃ.jpg"
+                      width={100}
+                      height={100}
+                      className={styles.sec3_ImageBox}
+                      alt={"野菜画像"}
+                    />
+                    <div>
+                      <p>商品名:{name(item.item_id)}</p>
+                    </div>
+                    <div>
+                      <p>価格:{price(item.item_id)}</p>
+                    </div>
 
-                  <div>
-                    <label htmlFor={item.id}>
-                      数量変更：
-                      <select id={item.id} defaultValue={count}>
-                        <option value="0">0</option>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                        <option value="6">6</option>
-                        <option value="7">7</option>
-                        <option value="8">8</option>
-                        <option value="9">9</option>
-                        <option value="10">10</option>
-                      </select>
-                    </label>
+                    <div>
+                      <label htmlFor={item.id}>
+                        数量変更：
+                        <select
+                          id={item.id}
+                          defaultValue={count}
+                          onChange={(event) => changeItemQuantity(event)}
+                        >
+                          <option value="0">0</option>
+                          <option value="1">1</option>
+                          <option value="2">2</option>
+                          <option value="3">3</option>
+                          <option value="4">4</option>
+                          <option value="5">5</option>
+                          <option value="6">6</option>
+                          <option value="7">7</option>
+                          <option value="8">8</option>
+                          <option value="9">9</option>
+                          <option value="10">10</option>
+                        </select>
+                      </label>
+                    </div>
+                    <div>
+                      <button onClick={() => deleteCartItem(item.item_id)}>
+                        削除
+                      </button>
+                    </div>
                   </div>
-                  <div>
-                    <button onClick={() => deleteCartItem()}>削除</button>
-                  </div>
+                  {/* 下記から小計表示と購入手続きボタン */}
                 </div>
-                {/* 下記から小計表示と購入手続きボタン */}
-              </div>
+              </form>
             </>
           );
         }
