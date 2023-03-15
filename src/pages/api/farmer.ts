a76d6277503ca03d45394645786ea5530d8259a1
Takao Shishido <takao.shishido@rakus-partners.co.jp>
2023-03-15 14:34:00 +0900

    fetch部分の環境変数設定

--- src/pages/api/farmer.ts
+++ src/pages/api/farmer.ts
@@ -10,11 +10,17 @@ export default async function handler(
 ) {
   const { search } = req.query;
 
-  const url = `http://127.0.0.1:8000/farmer_data?select=*,items!inner(*)&items.items_search=like.*${search}*`;
+  const url = `${process.env["NEXT_PUBLIC_URL"]}/farmer_data?select=*,items!inner(*)&items.items_search=like.*${search}*`;
   const options = {
     method: "GET",
+    headers: {
+      apikey: `${process.env["NEXT_PUBLIC_DB_KEY"]}`,
+      Authorization: `Bearer ${process.env["NEXT_PUBLIC_DB_KEY"]}`,
+    },
   };
   const response = await fetch(url, options);
   const data = await response.json();
   res.status(200).json(data);
+  // データ確認用
+  // console.log(data);
 }
