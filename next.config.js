60a91fc6d9d562a60f27298cca68f50f54bc86b4
Toya Manabu <manabu.toya@rakus-partners.co.jp>
2023-03-16 09:52:14 +0900

    createMiddleweare

--- next.config.js
+++ next.config.js
@@ -4,12 +4,3 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
-
-// module.exports = {
-//   reactStrictMode: false,
-//   env: {
-//     ENABLE_BASIC_AUTH: process.env.ENABLE_BASIC_AUTH,
-//     BASIC_AUTH_USER: process.env.BASIC_AUTH_USER,
-//     BASIC_AUTH_PASSWORD: process.env.BASIC_AUTH_PASSWORD,
-//   },
-// };
