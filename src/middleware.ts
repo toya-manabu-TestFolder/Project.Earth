ae4d79de86bc382d43bdfc37e778e2c71363f22e
Toya Manabu <manabu.toya@rakus-partners.co.jp>
2023-03-17 12:05:55 +0900

    tst

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 matcher : middleware.jsを適用する（呼び出す）パスを指定する
 */
 export const config = {
-  matcher: ["/:path*", "/api/:path*"],
+  matcher: ["/"],
 };
 
 export function middleware(req: NextRequest) {
